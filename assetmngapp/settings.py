5e7510edd9a6fded3eaa2963037a42a633657307
likithkun <95670018+likithkun@users.noreply.github.com>
2023-09-22 15:12:04 +0530

    Update settings.py

--- assetmngapp/settings.py
+++ assetmngapp/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
