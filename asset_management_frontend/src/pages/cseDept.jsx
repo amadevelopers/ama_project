f070a0c0ecc973795cf3315c86ec03b19b742af7
Rishithp06 <rishithp2003@gmail.com>
2023-10-10 22:42:56 +0530

    layout done

--- asset_management_frontend/src/pages/cseDept.jsx
+++ asset_management_frontend/src/pages/cseDept.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import '../css/cseDept.css'
 
-function CseDept() {
+
+const cseDept = () => {
   return (
-    <div><h1>Hello</h1>CSE DEPT</div>
+    <div className='main-div'>
+
+
+      <table>
+        <thead>
+            <tr>
+              <th>Name</th>
+              <th>ID</th>
+              <th>Quantity</th>
+              <th>Purchase Date</th>
+              <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+              <tr>
+                <td>Projector</td>
+                <td>123456</td>
+                <td>20</td>
+                <td>10/10/23</td>
+                <td>20000</td>
+                <td>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Projector</td>
+                <td>123456</td>
+                <td>20</td>
+                <td>10/10/23</td>
+                <td>20000</td>
+                <td>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+
+             
+
+             
+        </tbody>
+      </table>
+        
+      
+    </div>
   )
 }
 
-export default CseDept
+export default cseDept
