4ea1cbba52211a5896c3a7f468b8372ea89e09d7
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-10-11 05:49:52 +0530

    pulling the changes

--- asset_management_frontend/src/routes/appRoutes.js
+++ asset_management_frontend/src/routes/appRoutes.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "../pages/homePage";
+import LoginPage from '../pages/loginPage';
 function AppRoutes() {
     return (
         <div>
             <Routes>
-                <Route path="/home"  element={<HomePage/>} />
+                <Route path="/"  element={<HomePage/>} />
+                <Route path="/login"  element={<LoginPage/>} />
             </Routes>
         </div>
     )
