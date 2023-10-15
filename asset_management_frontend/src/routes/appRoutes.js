14e52ba66cb7c953b439ef3ccb7e33516edb5c81
ANAGHA-N13 <anagha2003.13@gmail.com>
2023-10-15 13:04:42 +0530

    routes changed

--- asset_management_frontend/src/routes/appRoutes.js
+++ asset_management_frontend/src/routes/appRoutes.js
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import HomePage from "../pages/homePage";
 import LoginPage from '../pages/loginPage';
-import CseDept from '../pages/cseDept';
+// import CseDept from '../pages/cseDept';
 import AdminHomePage from '../pages/adminHomePage';
 function AppRoutes() {
     return (
@@ -12,7 +12,7 @@ function AppRoutes() {
                 <Route path="/"  element={<HomePage/>} />
                 <Route path="/login"  element={<LoginPage/>} />
                 <Route path="/admin-home" element={<AdminHomePage/>}/>
-                <Route path="/cse"  element={<CseDept/>} />
+                {/* <Route path="/cse"  element={<CseDept/>} /> */}
             </Routes>
         </div>
     )
