2213578fbb7a89fe57f95d5e77ec86b970193ed0
Vaibhav G <vaibhavvishnu456@gmail.com>
2023-11-06 10:54:45 +0530

    purchase update1

--- asset_management_frontend/src/pages/purchase.jsx
+++ asset_management_frontend/src/pages/purchase.jsx
@@ -1,46 +1,49 @@
+import Header from '../components/header/header';
 import React from 'react';
 import '../css/purchase.css';
+import { useState } from "react";
 
-function purchase() {  
-    const handleSubmit = event => {
-   event.preventDefault();
-   alert('You have submitted the form.')
-     }
+function Purchase() {  
+  //   const handleSubmit = event => {
+  //  event.preventDefault();
+  //  alert('You have submitted the form.')
+  //    }
+  const [formData, setFormData] = useState({invoice: "",date:"",vendor: "",quantity:"",amount:""});
 
-  return (
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`invoice: ${formData.invoice}, date: ${formData.date}, vendor: ${formData.vendor}, quantity: ${formData.quantity}, amount: ${formData.amount}`
+    );
+  };
+  return ( 
     <div className="wrapper">
+      <Header/>
       <h1>Purchase</h1>
-      <form onSubmit={handleSubmit}>
-      <fieldset>
-         <label>
-           <p>Invoice Number</p>
-           <input name="Invoice Number" />
-         </label>
-         <label>
-           <p>Name</p>
-           <input name="name" />
-         </label>
-         <label>
-           <p>Vendor</p>
-           <input name="Vendor" />
-         </label>
-         <label>
-           <p>Date</p>
-           <input name="Date" />
-         </label>
-         <label>
-           <p>Quantity</p>
-           <input name="Quantity" />
-         </label>
-         <label>
-           <p>Total Amount</p>
-           <input name="Total Amount" />
-         </label>
-       </fieldset>
-       <button type="submit">Submit</button>
+      <form onSubmit={handleSubmit}> 
+      <label htmlFor="invoice">Invoice Number:</label>
+      <input type="text" id="invoice" name="invoice" value={formData.invoice} onChange={handleChange}/>
+
+      <label htmlFor="date">Date:</label>
+      <input type="text" id="date" name="date" value={formData.date} onChange={handleChange}/>
+
+      <label htmlFor="vendor">Vendor Name:</label>
+      <input type="text" id="vendor" name="vendor" value={formData.vendor} onChange={handleChange}/>
+
+      <label htmlFor="quantity">Quantity:</label>
+      <input type="number" id="quantity" name="quantity" value={formData.quantity} onChange={handleChange}/>
+
+      <label htmlFor="amount">Total Amount:</label>
+      <input type="number" id="amount" name="amount" value={formData.amount} onChange={handleChange}/>
+
+      <button type="submit">Submit</button>
       </form>
     </div>
   )
 }
 
-export default purchase;
+export default Purchase;
