12ad965f1a847a605e61a6ddd8e884a5adcd04dc
niranjan <niranjanchordate@gmail.com>
2024-01-19 16:19:10 +0000

    view-assets agthaaaaaaa ide

---
+++ asset_management_frontend/src/pages/roomData.js
@@ -0,0 +1,18 @@
+
+const roomData = [
+  {
+      "name": "computer science",
+      "building": {
+          "name": "cse block"
+      }
+  },
+  {
+      "name": "info science",
+      "building": {
+          "name": "cse block"
+      }
+  }
+]
+  
+  export default roomData;
+  
